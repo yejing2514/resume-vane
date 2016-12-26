edf883c44923d4e8ea859add87c27d3ea54f7c70
yejing2514 <251458365@qq.com>
2016-12-26 21:27:29 +0800

    提交基本完成功能

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -72,11 +72,30 @@ $(".enterBtn").tap(function() {
 	$("#mainContainer").show();
 
 	$.post('http://localhost:8000/skill', function(data) {
-		console.log(data);
+		var str = '';
+		for(var i = 0; i < data.length; i++) {
+
+			str += '<li>' +
+				'<div class="mainLogo">' +
+				'<img src="' + data[i].img + '" />' +
+				'</div>' +
+				'<div class="intro">' +
+				'<p class="introTitle"><strong>Category：</strong><span>' + data[i].category + '</span></p>' +
+				'<p class="introMain"><strong>Name：</strong><span>' + data[i].name + '</span></p>' +
+				'<p class="introMain"><strong>Time：</strong><span>' + data[i].time + '</span></p>' +
+				'<p class="introMain"><strong>level：</strong><span>' + data[i].level + '</span></p>' +
+				'</div>' +
+				'</li>';
+
+		}
+
+		$('.swiper-slide ul').eq(0).html(str)
+
 		var myScroll;
 		myScroll = new IScroll('#wrapper', {
-			mouseWheel: true
-		});
+			mouseWheel: true,
+			scrollX: false,
+		})
 		document.addEventListener('touchmove', function(e) {
 			e.preventDefault();
 		}, false);
@@ -89,11 +108,132 @@ $(".enterBtn").tap(function() {
 			},
 			onSlideChangeEnd: function(swiper) {
 				swiperAni.swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+				var num = swiper.activeIndex;
+				$('#footer li').eq(num).addClass('active').siblings('li').removeClass('active')
+				myScroll.refresh();
+				myScroll.scrollTo(0, 0);
+
+				if(num == 3) {
+		
+					$('.bgline span').eq(0).animate({
+						color: '#F34C1D'
+					}, 200, 'linear', function() {
+						$('.bgline span').eq(1).animate({
+							color: '#F34C1D'
+						}, 200, 'linear', function() {
+							$('.bgline span').eq(2).animate({
+								color: '#F34C1D'
+							}, 200, 'linear', function() {
+
+								$('.bgline span').eq(3).animate({
+									color: '#F34C1D'
+								}, 200, 'linear', function() {
+									
+									$('.bgline span').eq(4).animate({
+						color: '#F34C1D'
+					}, 200, 'linear', function() {
+						
+						
+							$('.bgline span').eq(5).animate({
+						color: '#F34C1D'
+					}, 200, 'linear', function() {
+						
+							$('.bgline span').eq(6).animate({
+						color: '#F34C1D'
+					}, 200, 'linear', function() {})
+						
+					})
+						
+					})
+									
+									
+								})
+							})
+
+						})
+
+					})
+
+				}
 
 			}
 
 		});
 
+		$('#footer li').tap(function() {
+			num = $(this).index()
+			$('#footer li').eq(num).addClass('active').siblings('li').removeClass('active')
+			swiper.slideTo(num, 500, true);
+			myScroll.scrollTo(0, 0);
+			myScroll.refresh();
+
+		})
+
+	})
+
+	$.post('http://localhost:8000/project', function(data) {
+
+		var str = '';
+		for(var i = 0; i < data.length; i++) {
+
+			str += '<li>' +
+				'<div class="mainLogo">' +
+				'<img src="' + data[i].img + '">' +
+				'</div>' +
+				'<div class="intro">' +
+				'<p class="introTitle"><strong>Category：</strong><span>' + data[i].category + '</span></p>' +
+				'<p class="introMain"><strong>Name：</strong><span>' + data[i].name + '</span></p>' +
+				'<p class="introMain"><strong>Url：</strong><span>' + data[i].url + '</span></p>' +
+				'<p class="introMain"><strong>Description：</strong><span>' + data[i].description + '</span></p>' +
+				'<p class="introMain"><strong>Detail：</strong><span>' + data[i].detail + '</span></p>' +
+				'<p class="introMain"><strong>Tech：</strong><span>' + data[i].tech + '</span></p>' +
+				'</div></li>';
+
+		}
+
+		$('.swiper-slide ul').eq(1).html(str)
+
+	})
+
+	$.post('http://localhost:8000/work', function(data) {
+
+		var str = '';
+		for(var i = 0; i < data.length; i++) {
+
+			str += '<li>' +
+				'<div class="mainLogo">' +
+				'<img src="' + data[i].img + '">' +
+				'</div>' +
+				'<div class="intro">' +
+				'<p class="introTitle"><strong>Category：</strong><span>' + data[i].category + '</span></p>' +
+				'<p class="introMain"><strong>Name：</strong><span>' + data[i].name + '</span></p>' +
+				'<p class="introMain"><strong>Url：</strong><span>' + data[i].url + '</span></p>' +
+				'<p class="introMain"><strong>Time：</strong><span>' + data[i].time + '</span></p>' +
+				'<p class="introMain"><strong>Posts：</strong><span>' + data[i].posts + '</span></p>' +
+				'<p class="introMain"><strong>Reportto：</strong><span>' + data[i].reportto + '</span></p>' +
+				'<p class="introMain"><strong>Peoples：</strong><span>' + data[i].peoples + '</span></p>' +
+				'<p class="introMain"><strong>Projects：</strong><span>' + data[i].projects + '</span></p>' +
+				'</div></li>';
+
+		}
+
+		$('.swiper-slide ul').eq(2).html(str)
+
 	})
 
 })
+var music=document.getElementById("music")
+
+	$('.musicConte').tap(function(){
+		
+		if(music.paused){
+			$('#change').addClass('anMuci')
+			music.play();
+		}else{
+			music.pause();
+			$('#change').removeClass('anMuci')
+				
+		}
+
+		
+	})
